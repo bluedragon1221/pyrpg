6a91ef8acbdb5015cb73351a53d109966847b14c
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2024-06-27 08:48:00 -0500

    fix main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from lib.container import Object, Player
-from lib.environment import CurrentEnvironment, Environment
+from lib.container import Object, Weapon
+from lib.character import Player
+from lib.environment import Environment
 
 player = Player("Collin")
 
@@ -8,8 +9,6 @@ middle_meadow.set_text("""You arive in a meadow, surounded by a dense, lushous f
 To the left, you see a patch of flowers.
 To the right there is a dark, cluster of trees that almost looks like it's hiding something...""")
 
-env = CurrentEnvironment()
-
 # --- Flower Patch
 
 flower_patch = Environment("flower patch")
@@ -17,32 +16,25 @@ flower_patch.set_text("""You walk over to the patch of flowers.
 They are beautiful and you can't help but wonder if your mom would like them.
 Do you want to pick them?""")
 
+@flower_patch.command("Pick them")
+def pick_flowers():
+    print("You pick the flowers")
+    flowers = Object("flowers x5")
+    player.give_item(flowers)
+
+    flower_patch.rm_command("Pick them")
+    flower_patch.set_text("You walk over to where you picked the patch of flowers.")
+    
+    flower_patch.show_menu()
+
+@flower_patch.command("Go back to the meadow")
+def dont_pick_flowers():
+    print("You turn around and head back to the middle of the meadow")
+    middle_meadow.show_menu()
 
 @middle_meadow.command("Go to the patch of flowers")
 def patch_of_flowers():
-    env.set_environment(flower_patch)
-
-    # Override the command so that you can't pick up the flowers again
-    @middle_meadow.command("Go to the patch of flowers")
-    def no_more_flowers():
-        print("""There are no more flowers. You picked them all!
-You turn around and head back to the middle of the meadow""")
-        env.set_environment(middle_meadow)
-
-
-@flower_patch.command("Pick them")
-def pick_flowers():
-    print("You pick the flowers and head back to the middle of the meadow")
-    flowers = Object.new_item("flowers x5")
-    player.give_item(flowers)
-    env.set_environment(middle_meadow)
-
-
-@flower_patch.command("Don't")
-def dont_pick_flowers():
-    print("You turn around and head back to the middle of the meadow")
-    env.set_environment(middle_meadow)
-
+    flower_patch.show_menu()
 
 # --- Tree Cluster
 
@@ -54,26 +46,27 @@ Do you want to take it?""")
 
 @middle_meadow.command("Go to the dark cluster of trees")
 def trees():
-    env.set_environment(tree_cluster)
+    tree_cluster.show_menu()
 
+@middle_meadow.command("Quit")
+def exit_game():
+    print("Thanks for playing!")
 
 @tree_cluster.command("Take it")
 def take_it():
-    print("You stuff the sword in your backpack and return to the middle of the meadow")
-    sword = Object.new_weapon("rusty sword")
+    print("You stuff the sword in your backpack")
+    sword = Weapon("rusty sword", 3)
     player.give_item(sword)
 
+    tree_cluster.rm_command("Take it")
+    tree_cluster.set_text("Now you're at the cluster of trees, where you picked up the sword")
+    tree_cluster.show_menu()
 
-@tree_cluster.command("Dont")
+
+@tree_cluster.command("Turn back to the meadow")
 def dont_take_it():
-    print(
-        "You don't take the sword, instead returning back to the middle of the meadow"
-    )
+    print("You return back to the middle of the meadow")
+    middle_meadow.show_menu()
 
 
-if __name__ == "__main__":
-    env.set_environment(middle_meadow)
-    while True:
-        if not env.get_environment().show_menu("What do you do?"):
-            print("Thanks for playing!")
-            break
+middle_meadow.show_menu()
